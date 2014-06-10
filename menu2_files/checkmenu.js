412d7733b86fb24b9ab1d8d5248857ef92d7ee09
huweixuan <weixuan.hu@dianping.com>
2014-06-10 09:56:18 +0800

    mask

--- menu2_files/checkmenu.js
+++ menu2_files/checkmenu.js
@@ -1,8 +1,11 @@
 function checkMenu () {
   var list = _q('#checkMenu');
+  var mask = _q('#mask');
   if (list.style.display == 'none') {
     list.style.display = 'block';
+    mask.style.display = 'block';
   } else {
     list.style.display = 'none';
+    mask.style.display = 'none';
   }
 }
